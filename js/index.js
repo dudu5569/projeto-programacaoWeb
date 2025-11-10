05dade3000e714d8dba6207f737601748712c733
Duh <wakaw5302@gmail.com>
2025-11-10 16:02:46 -0300

    adicionando local storage

--- js/index.js
+++ js/index.js
@@ -2,6 +2,21 @@ document.getElementById("botao_submit").addEventListener("click", function(){
     var descricao_tarefa = document.getElementById("descricao-tarefa").value;
     var categoria_tarefa = document.getElementById("cat_tarefa").value;
     var importancia_tarefa = document.getElementById("importancia-tarefa").value;
+    var responsavel_tarefa = document.getElementById("responsavel-tarefa").value;
+
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+    let novaTarefa = {
+        descricao: descricao_tarefa,
+        categoria: categoria_tarefa,
+        importancia: importancia_tarefa,
+        responsavel: responsavel_tarefa
+    };
+
+    tarefas.push(novaTarefa);
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    
 
     let template = document.getElementById("tarefas_a_fazer");
 
@@ -12,22 +27,26 @@ document.getElementById("botao_submit").addEventListener("click", function(){
     nova_tarefa.querySelector(".descricao_tarefa_afazer").textContent = descricao_tarefa;
     nova_tarefa.querySelector(".categoria_tarefa_afazer").textContent = categoria_tarefa;
     nova_tarefa.querySelector(".importancia_tarefa_afazer").textContent = importancia_tarefa;
+    nova_tarefa.querySelector(".responsavel_tarefa_afazer").textContent = responsavel_tarefa;
+
 
     let btn_deletar = nova_tarefa.querySelector(".delete");
     btn_deletar.addEventListener("click", function(){
     nova_tarefa.remove();
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    tarefas = tarefas.filter(t => t.descricao !== nova_tarefa.querySelector(".descricao_tarefa_afazer").textContent);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
     });
 
     document.getElementById("tarefas").appendChild(nova_tarefa);   
 
+    document.getElementById("responsavel-tarefa").value = "";
     document.getElementById("descricao-tarefa").value = "";
     document.getElementById("cat_tarefa").value = "";
     document.getElementById("importancia-tarefa").value = "";
 
+    
+
 });
 
 
-let btn_deletar = nova_tarefa.querySelector(".delete");
-btn_deletar.addEventListener("click", function(){
-    nova_tarefa.remove();
-});
